e699ea7ee0550d5da1b6932e4081dc22732e62bf
Ronald Hamilton Figueroa Patriz <fp23010@ues.edu.sv>
2026-09-10 20:40:04 -0600

    Initial commit

---
+++ src/main/java/sv/edu/ues/occ/ingenieria/ppi115_2026/salud/guia02/fp23010/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package sv.edu.ues.occ.ingenieria.ppi115_2026.salud.guia02.fp23010;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
